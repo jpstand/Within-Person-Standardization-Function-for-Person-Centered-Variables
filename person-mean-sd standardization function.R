be4ea0e2fb27da1477e5196ee94076db73b4c226
jpstand <jstandring7@gmail.com>
2026-08-16 01:53:23 -0400

    added support for non centered L2 vars
    you can now standardize non centered level 2 (i.e., between-person variables)

--- person-mean-sd standardization function.R
+++ person-mean-sd standardization function.R
@@ -1,6 +1,8 @@
 
-ps_standardization <- function(df, wp_vec = c(), bp_vec = c(), id, lag_bool = FALSE, lead_bool = FALSE, 
+ps_standardization <- function(df, wp_vec = c(), bp_vec = c(), id, non_c_bp_vars = c(),
+                               lag_bool = FALSE, lead_bool = FALSE, 
                                lag_n = 1L, lead_n = 1L, lag_vec = c(), lead_vec = c()) {
+  
   #this function assumes (1) your wp_vec and bp_vec will contain within-person and between-person components 
   #of person-centered variables, respectively, and (2) your data is already temporally arranged.
   
@@ -11,6 +13,11 @@ ps_standardization <- function(df, wp_vec = c(), bp_vec = c(), id, lag_bool = FA
   #if you did not already filter your df to exclude your id var based on a minimum observation requirement
   #the function will remove those with only one observation because standard deviation of within-person
   #components can not be computed
+
+  #non_c_bp_vars is for non centered between-person (i.e., level 2) variables. For example, 
+  #if you want to control for and standardize a variable containing the age of participants, but the variable
+  #has not yet been centered, you can add the name to non_centered_bp_vars
+  
   one_obs_ids <- df |>
     dplyr::count(.data[[id]]) |>
     dplyr::filter(n == 1) |>
@@ -45,6 +52,21 @@ ps_standardization <- function(df, wp_vec = c(), bp_vec = c(), id, lag_bool = FA
     df <- df |>
       dplyr::mutate(!!bp_z := .data[[bp_var]] / global_sd)
     
+  }
+
+  for (non_c_bp_var in non_c_bp_vars) {
+    non_c_bp_var_z = paste0(non_c_bp_var, "_z")
+    
+    global_mean_and_sd <- df |>
+      dplyr::group_by(.data[[id]]) |>
+      dplyr::slice(1) |>
+      dplyr::ungroup() |>
+      dplyr::summarise(mean = mean(.data[[non_c_bp_var]], na.rm = TRUE),
+                       sd = sd(.data[[non_c_bp_var]], na.rm = TRUE))
+    
+    df <- df |>
+      dplyr::mutate(!!non_c_bp_var_z := (.data[[non_c_bp_var]] - global_mean_and_sd$mean) / 
+                      global_mean_and_sd$sd)
   }
   
   if (lag_bool == TRUE) {
